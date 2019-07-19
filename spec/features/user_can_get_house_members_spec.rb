8fcf95a96f6b0fdfa1da5d97a78f6026cab00669
earl-stephens <stephensearl@hotmail.com>
2019-07-19 09:26:12 -0600

    setup gemfile and wrote test

---
+++ spec/features/user_can_get_house_members_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe 'a user can get info for', type: :feature do
+  it 'all of the greyjoy house members' do
+# As a user
+# When I visit "/"
+    visit '/'
+# And I select "Greyjoy" from the dropdown
+    fill_in :q, with: 'Greyjoy'
+# And I click on "Get Members"
+    click_on 'Get Members'
+# Then my path should be "/search" with "house=greyjoy" in the parameters
+    expect(current_path).to eq('/search')
+    expect(current_url).to include('house=greyjoy')
+# And I should see a message "7 Members"
+    expect(page).to have_content('7 Members')
+# And I should see a list of the 7 members of House Greyjoy
+    expect(page).to have_css('.house_members', count: 7)
+# And I should see a name and id for each member.
+    within(first('.house_members')) do
+      expect(page).to have_css('.house_member_name')
+      expect(page).to have_css('.house_member_id')
+    end
+  end
+end
